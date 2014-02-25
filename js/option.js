ea9791d0bc025394120c519d6e3fd0745501f89a
McSherry <liamj.mcsherry@googlemail.com>
2014-02-25 20:03:45 +0000

    most of edit JS implemented
    still need to write function to store the edits. This will probably be in next commit.

--- js/option.js
+++ js/option.js
@@ -81,13 +81,69 @@ function removeRuleFromStore() {
     });
 }
 
+function resetInputFields() {
+    $("#si_addRuleInputsCtnr *").val("");
+}
+
+function cancelEditOp() {
+    $("#si_addUrlButton")
+        .val("Add");
+        //.on("click", addRuleToBeStored);
+    
+    console.log("pre enable");
+    $("#si_addUrlBox")
+        .val("");
+    console.log("post enable");
+    
+    $("#si_resetUrlButton")
+        .off("click");
+        //.on("click", resetInputFields);
+}
+
+function editRuleFromStore() {
+    var editItem = $("#si_rulesList").val();
+    
+    if (typeof editItem !== "undefined") {
+        $("#si_editSetRule").prop("disabled", true);
+        $("#si_removeSetRule").prop("disabled", true);
+        $("#si_addUrlBox").prop("disabled", true);
+        
+        editItem = editItem[0];
+    
+        $("#si_addUrlButton").val("Save Edit");
+        $("#si_resetUrlButton").val("Cancel Edit");
+        
+        $("#si_resetUrlButton").off("click").on("click", cancelEditFromStore);
+        
+        $("#si_addUrlBox").val(editItem);
+        chrome.storage.local.get("rules", function(ret) {
+            ret["rules"].forEach(function(i) {
+                if (i["domain"] === editItem) {
+                    $("#si_cssRulesBox").val(i["rule"]);
+                }
+            });
+        });
+    }
+}
+function cancelEditFromStore() {
+    $("#si_editSetRule").prop("disabled", false);
+    $("#si_removeSetRule").prop("disabled", false);
+    $("#si_addUrlBox").prop("disabled", false);
+    
+    $("#si_addUrlButton").val("Add");
+    $("#si_resetUrlButton").val("Reset");
+    
+    $("#si_addUrlButton").off("click").on("click", addRuleToBeStored);
+    $("#si_resetUrlButton").off("click").on("click", resetInputFields);
+    resetInputFields();
+}
+
 $(function() {
     createDefaultRuleStore();
     loadStoredRulesToSelectBox();
     
     $("#si_addUrlButton").on("click", addRuleToBeStored);
     $("#si_removeSetRule").on("click", removeRuleFromStore);
-    $("#si_resetUrlButton").on("click", function() {
-        $("#si_addRuleInputsCtnr *").val("");
-    });
+    $("#si_resetUrlButton").on("click", resetInputFields);
+    $("#si_editSetRule").on("click", editRuleFromStore);
 });
