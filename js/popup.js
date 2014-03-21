995b1f4d7469497ebe971170fb839fb1c2829d82
McSherry <liamj.mcsherry@googlemail.com>
2014-03-21 19:12:17 +0000

    add JS for navigation via popup to injector.io

--- js/popup.js
+++ js/popup.js
@@ -2,4 +2,8 @@ $(function() {
     $("#options").on("click", function() {
         chrome.tabs.create({"url": "options.html"});
     });
+    
+    $(".website").on("click", function() {
+        chrome.tabs.create({"url": "http://injector.io"});
+    });
 });
